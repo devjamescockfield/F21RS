0df839edfc99563243d6bed1e949a40ffdd29d19
devjamescockfield <jamescockfield10@gmail.com>
2023-10-15 15:22:54 +0100

    upload code

--- CW/console.ads
+++ CW/console.ads
@@ -10,37 +10,37 @@ is
    pragma Elaborate_Body;
    
    IndicatorState: Boolean;
-   AutoState:      Boolean; -- Set to True when Auto_Mode enabled and False when Manual_Mode enabled.
+   AutoState: Boolean; -- Set to True when Auto_Mode enabled and False when Manual_Mode enabled.
    
    procedure Enable_Indicator
    with
-     Global => (Output => IndicatorState),
-     Depends => (IndicatorState => null);
+       Global => (Output => IndicatorState),
+       Depends => (IndicatorState => null);
 
    procedure Disable_Indicator
    with
-     Global => (Output => IndicatorState),
-     Depends => (IndicatorState => null);
+       Global => (Output => IndicatorState),
+       Depends => (IndicatorState => null);
    
    function Indicator_Enabled return Boolean
    with
-     Global => (Input => IndicatorState),
-     Depends => (Indicator_Enabled'Result => IndicatorState);
+       Global => (Input => IndicatorState),
+       Depends => (Indicator_Enabled'Result => IndicatorState);
    
    procedure Enable_Auto_Mode
    with
-     Global => (Output => AutoState),
-     Depends => (AutoState => null);
+       Global => (Output => AutoState),
+       Depends => (AutoState => null);
 
    procedure Disable_Auto_Mode
    with
-     Global => (Output => AutoState),
-     Depends => (AutoState => null);
+       Global => (Output => AutoState),
+       Depends => (AutoState => null);
 
    function Auto_Mode_Enabled return Boolean
    with
-     Global => (Input => AutoState),
-     Depends => (Auto_Mode_Enabled'Result => AutoState);
+       Global => (Input => AutoState),
+       Depends => (Auto_Mode_Enabled'Result => AutoState);
    
 end Console;
 
